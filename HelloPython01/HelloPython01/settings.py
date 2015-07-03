2abfa651c06457b4fcbe75fe5af67c72289caffa
松田＠デジモ <koichi.matsuda@digimo.co.jp>
2015-07-03 16:08:04 +0900

    add settings of template dir

--- HelloPython01/HelloPython01/settings.py
+++ HelloPython01/HelloPython01/settings.py
@@ -102,3 +102,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# template files
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+TEMPLATE_DIRS = (
+    os.path.join(PROJECT_ROOT, 'templates')
+)
